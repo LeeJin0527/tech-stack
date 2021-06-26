36eebc774b8cc4acf20fe488d686301b990cd930
JIN <64263694+LeeJin0527@users.noreply.github.com>
2021-06-26 12:47:22 +0900

    Add files via upload

--- 기본 프로그래밍 구조/src/MyStack.java
+++ 기본 프로그래밍 구조/src/MyStack.java
@@ -1,40 +1,41 @@
-package Collection;
-
+package Generic;
 class GStack<T>{
     int tos;
-    Object[] stck;
+    Object [] stck; //스택에 저장된 요소의 개수
     public GStack(){
         tos = 0;
         stck = new Object[10];
     }
-    public void push (T item){
-        if ( tos == 10)
-            return;
+    public void push(T item){
+        if (tos == 10)
+            return ;
         stck[tos] = item;
-        tos ++;
+        tos++;
     }
     public T pop(){
-        if (tos == 0)
+        if (tos == 0 )
             return null;
         tos --;
         return (T)stck[tos];
     }
+
 }
 public class MyStack {
     public static void main(String[] args){
-        GStack <String> stringGStack = new GStack<String>();
-        stringGStack.push("seoul");
-        stringGStack.push("pusan");
-        stringGStack.push("LA");
-        for ( int n = 0; n < 3; n++)
-            System.out.println(stringGStack.pop());
+        GStack<String> stringStack = new GStack<String>();
+        stringStack.push("Seoul");
+        stringStack.push("busan");
+        stringStack.push("LA");
+
+        for( int n = 0; n < 3; n++)
+            System.out.println(stringStack.pop());
 
         GStack<Integer> intStack = new GStack<Integer>();
-        intStack.push(1);
-        intStack.push(2);
         intStack.push(3);
-
-        for(int i=0 ;i<3; i++)
+        intStack.push(5);
+        for (int i=0; i<2; i++){
             System.out.println(intStack.pop());
+        }
+
     }
 }
